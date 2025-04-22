498c4672065121b6598790f00f0038a08860a276
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-04-22 11:02:03 -0300

    feat:addint nextAuth with credentials provider

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,9 +1,12 @@
 "use server";
 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 import { z } from "zod";
+
 const sql = postgres(process.env.POSTRESS_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
@@ -33,6 +36,25 @@ export type State = {
   };
   message?: string | null;
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.name) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
 
 /**
  * Creates an invoice in the database.
